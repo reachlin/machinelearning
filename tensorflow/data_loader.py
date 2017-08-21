47ee25cbaf2d1ba15a8dd86760015c60cad2d7bf
reachlin <reachlin@gmail.com>
2017-08-21 16:56:14 +0800

    eval

--- tensorflow/data_loader.py
+++ tensorflow/data_loader.py
@@ -14,14 +14,14 @@ def get_img(path):
   im.convert('1')
   return list(im.getdata())
 
-def get_data(wanted_labels, onehot=True):
+def get_data(wanted_labels, folders=['data'], onehot=True):
   # 0, 1, 2, ..., 9
   chinese_num = ["0x96f6", "0x4e00", "0x4e8c", "0x4e09", "0x56db",
     "0x4e94", "0x516d", "0x4e03", "0x516b", "0x4e5d"]
   images = []
   labels = []
   for wanted in wanted_labels:
-    for folder in ["data", "data/light", "data/hard1", "data/hard2"]:
+    for folder in folders:
       images.append(get_img("%s/%s.bmp" % (folder, chinese_num[wanted])))
       if onehot:
         # label must be in one-hot format
@@ -32,8 +32,8 @@ def get_data(wanted_labels, onehot=True):
         labels.append(wanted)
   return images, labels
 
-def get_train_data(onehot=True):
-  return get_data([0,1,2,3,4,5,6,7,8,9], onehot)
+def get_train_data(folders=["data", "data/hard1", "data/hard2"], onehot=True):
+  return get_data([0,1,2,3,4,5,6,7,8,9], folders, onehot)
 
-def get_test_data(onehot=True):
-  return get_data([0,4,3,7,0,0,1,2,5,9,8,8], onehot)
+def get_test_data(folders=["data/light"], onehot=True):
+  return get_data([0,4,3,7,0,0,1,2,5,9,8,8], folders, onehot)
