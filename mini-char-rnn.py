98e61a78506c74d6ed790d7c3c27a82df62c88b5
CaiLin <cailin@CaiLindeMac-mini.local>
2017-09-06 23:01:58 +0800

    m

--- mini-char-rnn.py
+++ mini-char-rnn.py
@@ -5,8 +5,9 @@ BSD License
 import numpy as np
 import sys
 
+
 # data I/O
-data = open(sys.argv[1], 'r').read() # should be simple plain text file
+data = open(sys.argv[1], 'r').read().decode("UTF-8") # should be simple plain text file
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
 print 'data has %d characters, %d unique.' % (data_size, vocab_size)
