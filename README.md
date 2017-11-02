eb8ba02aa0da86ccf9991fa609afa84d8c180a21
lincai <lincai@cn.ibm.com>
2017-11-02 10:38:08 +0800

    org

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # machinelearning
-machinelearning stuff
+machine learning algorithm sample code. Most of them are python. `tensorflow` folder includes implementation using tensorflow. 
 
-## sample_cnn.py
-A simple chinese number image classifier using CNN. It reads black and white bitmap images containing a number in Chinese and outputs its arabic number, e.g. 七 as 7. The bitmap size must be 128 by 128.
+## tensorflow/sample[1-9].py
+Sample code to explain tensorflow concepts.
+
+## tensorflow/sample_mnist.py
+Basic linear classification using tensorflow. Although this is simple algorithm, but it explains major steps in machine learning very clearly.
+
+## tensorflow/sample_cnn.py
+A simple image classifier using CNN. It is modified to read black and white bitmap images containing a number in Chinese and outputs its arabic number, e.g. 七 as 7. The bitmap size must be 128 by 128.
 
 ```
 docker run --name mlearn -v $PWD:/root/ -it gcr.io/tensorflow/tensorflow bash
@@ -18,3 +24,40 @@ Modify [AK's RNN gist](http://karpathy.github.io/2015/05/21/rnn-effectiveness/) 
 python mini-char-rnn.py -i input_chinese.txt -l 40
 python mini-char-rnn.py -i lottery.txt -l 30 -n -test 08
 ```
+
+Sample outputs of Chinese poems:
+
+```
+iter 10000, loss: 71.170911
+----
+ 时 
+赧郎长水月皆海。 
+帝扫莫兮巴死水，寒高宿云鸯鞭间径，有尔同东黄金，栗藏访梦流，山女苍空乌去时，谢放营高晖。 
+鸡为去浪淡被。 
+落君如日。萧光芙馥州，秋君欲人相。 
+郎花必地锦张鱼，飞之莫三云 
+----
+iter 11000, loss: 65.929996
+iter 12000, loss: 61.579649
+iter 13000, loss: 57.133800
+iter 14000, loss: 53.556183
+iter 15000, loss: 49.873506
+iter 16000, loss: 47.075554
+iter 17000, loss: 43.890813
+iter 18000, loss: 41.661809
+iter 19000, loss: 38.913145
+iter 20000, loss: 36.891988
+low loss, starting test now...
+----
+ 青虎，何德横贵绣。 
+六留君不独去，埋花数何骄。照师黩死萦支声。江酣落自望朝桃花上霜烟。 
+黄鹤楼耳金玉，胡有月舟似，翡歌看低又入海，鼓水出几闲。系失垂杨江，转明霜珠。 
+挥戈曲千愁欲行，功起楚清，然 
+----
+```
+
+## tensorflow/sample_rnn.py
+RNN with LSTM. TBC...
+
+## Useful links
+* [Backpropagation](http://colah.github.io/posts/2015-08-Backprop/)
--- tensorflow/sample_rnn.py
+++ tensorflow/sample_rnn.py
@@ -1,59 +1,6 @@
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-"""Example / benchmark for building a PTB LSTM model.
-
-Trains the model described in:
-(Zaremba, et. al.) Recurrent Neural Network Regularization
-http://arxiv.org/abs/1409.2329
-
-There are 3 supported model configurations:
-===========================================
-| config | epochs | train | valid  | test
-===========================================
-| small  | 13     | 37.99 | 121.39 | 115.91
-| medium | 39     | 48.45 |  86.16 |  82.07
-| large  | 55     | 37.87 |  82.62 |  78.29
-The exact results may vary depending on the random initialization.
-
-The hyperparameters used in the model:
-- init_scale - the initial scale of the weights
-- learning_rate - the initial value of the learning rate
-- max_grad_norm - the maximum permissible norm of the gradient
-- num_layers - the number of LSTM layers
-- num_steps - the number of unrolled steps of LSTM
-- hidden_size - the number of LSTM units
-- max_epoch - the number of epochs trained with the initial learning rate
-- max_max_epoch - the total number of epochs for training
-- keep_prob - the probability of keeping weights in the dropout layer
-- lr_decay - the decay of the learning rate for each epoch after "max_epoch"
-- batch_size - the batch size
-- rnn_mode - the low level implementation of lstm cell: one of CUDNN,
-             BASIC, or BLOCK, representing cudnn_lstm, basic_lstm, and
-             lstm_block_cell classes.
-
-The data required for this example is in the data/ dir of the
-PTB dataset from Tomas Mikolov's webpage:
-
-$ wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
-$ tar xvf simple-examples.tgz
-
-To run:
-
-$ python ptb_word_lm.py --data_path=simple-examples/data/
-
+"""
+Modified from: https://github.com/tensorflow/models/blob/master/tutorials/rnn/ptb/ptb_word_lm.py
+RNN with LSTM cells
 """
 from __future__ import absolute_import
 from __future__ import division
